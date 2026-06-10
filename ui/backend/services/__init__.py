c186603801fd5354099e883f3e04097b9c149543
Leo <1093964909@qq.com>
2026-06-10 17:33:57 +0800

    Split background task persistence service

--- ui/backend/services/__init__.py
+++ ui/backend/services/__init__.py
@@ -12,6 +12,7 @@ from ui.backend.services.game_delete_service import GameDeleteCoordinator
 from ui.backend.services.game_read_service import GameHistoryService, GameReadGateway
 from ui.backend.services.live_game_lifecycle import LiveGameLifecycleCoordinator
 from ui.backend.services.role_service import RoleService, RoleServiceStoreProtocol
+from ui.backend.services.task_persistence_service import TaskPersistenceService
 from ui.backend.services.task_service import BackgroundTaskServiceProtocol, TaskService
 
 __all__ = [
@@ -28,5 +29,6 @@ __all__ = [
     "LiveGameLifecycleCoordinator",
     "RoleService",
     "RoleServiceStoreProtocol",
+    "TaskPersistenceService",
     "TaskService",
 ]
